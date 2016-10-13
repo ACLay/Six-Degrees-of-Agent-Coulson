9d03f4c97f299321953264273995760de7e81f9a
Alex Lay <AlexChrisLay@gmail.com>
2016-10-13 20:32:47 +0100

    added CA:TFA, Item 47, and the Agent Carter one shot

--- js/connections.js
+++ js/connections.js
@@ -1,36 +1,381 @@
 function getConnectionData(){
 return {
   "characters": [
+    "Agent Blake", 
     "Agent Coulson", 
     "Agent Sitwell", 
+    "Arnim Zola", 
+    "Bennie Pollock", 
     "Bruce Banner", 
+    "Bucky Barnes", 
     "Christine Everhart", 
+    "Claire Weiss", 
+    "Colonel Philips", 
+    "Colonel Phillips", 
     "Darcy Lewis", 
+    "Dr Erskine", 
+    "Dum Dum Dugan", 
     "Erik Selvig", 
     "Fandral", 
     "Frigga", 
+    "Gabe Jones", 
     "Happy Hogan", 
     "Hawkeye", 
     "Heimdall", 
     "Hogun", 
     "Howard Stark", 
     "JARVIS", 
+    "Jacques Dernier", 
+    "James Falsworth", 
     "Jane Foster", 
+    "Jim Morita", 
     "Laufey", 
     "Loki", 
     "Maria Stark", 
     "Nick Fury", 
     "Obadiah Staine", 
     "Odin", 
+    "Peggy Carter", 
     "Pepper Potts", 
+    "Red Skull", 
     "Rhodey", 
+    "Senator Brandt", 
     "Sif", 
+    "Steve Rogers", 
     "Thor", 
     "Tony Stark", 
     "Volstagg", 
     "Yinsen"
   ], 
   "properties": [
+    {
+      "characters": [
+        "Dum Dum Dugan", 
+        "Howard Stark", 
+        "Peggy Carter"
+      ], 
+      "interactions": [
+        {
+          "desc": "Howard recruited Peggy to found SHIELD", 
+          "p1": "Peggy Carter", 
+          "p2": "Howard Stark"
+        }, 
+        {
+          "desc": "Howard introduced Dugan to the bikini", 
+          "p1": "Howard Stark", 
+          "p2": "Dum Dum Dugan"
+        }
+      ], 
+      "name": "Agent Carter"
+    }, 
+    {
+      "characters": [
+        "Arnim Zola", 
+        "Bucky Barnes", 
+        "Colonel Philips", 
+        "Colonel Phillips", 
+        "Dr Erskine", 
+        "Dum Dum Dugan", 
+        "Gabe Jones", 
+        "Howard Stark", 
+        "Jacques Dernier", 
+        "James Falsworth", 
+        "Jim Morita", 
+        "Nick Fury", 
+        "Peggy Carter", 
+        "Red Skull", 
+        "Senator Brandt", 
+        "Steve Rogers"
+      ], 
+      "interactions": [
+        {
+          "desc": "Steve and Peggy worked together in the SSR", 
+          "p1": "Steve Rogers", 
+          "p2": "Peggy Carter"
+        }, 
+        {
+          "desc": "Steve and Bucky are best pals", 
+          "p1": "Steve Rogers", 
+          "p2": "Bucky Barnes"
+        }, 
+        {
+          "desc": "Colonel Philips was in charge of the SSR when Steve joined", 
+          "p1": "Steve Rogers", 
+          "p2": "Colonel Philips"
+        }, 
+        {
+          "desc": "Steve fought against Red Skull's Hydra", 
+          "p1": "Steve Rogers", 
+          "p2": "Red Skull"
+        }, 
+        {
+          "desc": "Howard gave Steve his shield", 
+          "p1": "Steve Rogers", 
+          "p2": "Howard Stark"
+        }, 
+        {
+          "desc": "Dr Erskine gave Steve the super soldier serum", 
+          "p1": "Steve Rogers", 
+          "p2": "Dr Erskine"
+        }, 
+        {
+          "desc": "Nick welcomed Steve to the present day in times square", 
+          "p1": "Steve Rogers", 
+          "p2": "Nick Fury"
+        }, 
+        {
+          "desc": "Zola was with Red Skull when he fought Steve in the HYDRA factory", 
+          "p1": "Steve Rogers", 
+          "p2": "Arnim Zola"
+        }, 
+        {
+          "desc": "Dugan served under Cap in the howling commandos", 
+          "p1": "Steve Rogers", 
+          "p2": "Dum Dum Dugan"
+        }, 
+        {
+          "desc": "Gabe served under Cap in the howling commandos", 
+          "p1": "Steve Rogers", 
+          "p2": "Gabe Jones"
+        }, 
+        {
+          "desc": "Jim served under Cap in the howling commandos", 
+          "p1": "Steve Rogers", 
+          "p2": "Jim Morita"
+        }, 
+        {
+          "desc": "James served under Cap in the howling commandos", 
+          "p1": "Steve Rogers", 
+          "p2": "James Falsworth"
+        }, 
+        {
+          "desc": "Jacques served under Cap in the howling commandos", 
+          "p1": "Steve Rogers", 
+          "p2": "Jacques Dernier"
+        }, 
+        {
+          "desc": "Senator Brandt gave Steve his performing job", 
+          "p1": "Steve Rogers", 
+          "p2": "Senator Brandt"
+        }, 
+        {
+          "desc": "Bucky was with Steve when he was talking to Peggy in the London pub", 
+          "p1": "Peggy Carter", 
+          "p2": "Bucky Barnes"
+        }, 
+        {
+          "desc": "Colonel Phillips ran the SSR when Peggy worked there in WW2.", 
+          "p1": "Peggy Carter", 
+          "p2": "Colonel Phillips"
+        }, 
+        {
+          "desc": "Dugan and Carter were both at the planning meeting for the attack on Hydra's last base", 
+          "p1": "Peggy Carter", 
+          "p2": "Dum Dum Dugan"
+        }, 
+        {
+          "desc": "Jones and Carter were both at the planning meeting for the attack on Hydra's last base", 
+          "p1": "Peggy Carter", 
+          "p2": "Gabe Jones"
+        }, 
+        {
+          "desc": "Morita and Carter were both at the planning meeting for the attack on Hydra's last base", 
+          "p1": "Peggy Carter", 
+          "p2": "Jim Morita"
+        }, 
+        {
+          "desc": "Falsworth and Carter were both at the planning meeting for the attack on Hydra's last base", 
+          "p1": "Peggy Carter", 
+          "p2": "James Falsworth"
+        }, 
+        {
+          "desc": "Dernier and Carter were both at the planning meeting for the attack on Hydra's last base", 
+          "p1": "Peggy Carter", 
+          "p2": "Jacques Dernier"
+        }, 
+        {
+          "desc": "Peggy and the Senator were in the observation booth when Steve got the super serum", 
+          "p1": "Peggy Carter", 
+          "p2": "Senator Brandt"
+        }, 
+        {
+          "desc": "Bucky was with Steve when he spoke to Phillips on returning from the Hydra factory", 
+          "p1": "Bucky Barnes", 
+          "p2": "Colonel Phillips"
+        }, 
+        {
+          "desc": "Bucky was with Steve when he fought Red Skull while escaping the HYDRA factory", 
+          "p1": "Bucky Barnes", 
+          "p2": "Red Skull"
+        }, 
+        {
+          "desc": "Dr Zola experimented on Bucky at the Hydra factory", 
+          "p1": "Bucky Barnes", 
+          "p2": "Arnim Zola"
+        }, 
+        {
+          "desc": "Bucky and Dugan served together in the howling commandos", 
+          "p1": "Bucky Barnes", 
+          "p2": "Dum Dum Dugan"
+        }, 
+        {
+          "desc": "Bucky and Gabe served together in the howling commandos", 
+          "p1": "Bucky Barnes", 
+          "p2": "Gabe Jones"
+        }, 
+        {
+          "desc": "Bucky and Jim served together in the howling commandos", 
+          "p1": "Bucky Barnes", 
+          "p2": "Jim Morita"
+        }, 
+        {
+          "desc": "Bucky and James served together in the howling commandos", 
+          "p1": "Bucky Barnes", 
+          "p2": "James Falsworth"
+        }, 
+        {
+          "desc": "Bucky and Jacques served together in the howling commandos", 
+          "p1": "Bucky Barnes", 
+          "p2": "Jacques Dernier"
+        }, 
+        {
+          "desc": "Stark and Phillips were together when Steve got the super serum", 
+          "p1": "Colonel Phillips", 
+          "p2": "Howard Stark"
+        }, 
+        {
+          "desc": "Erskine and Phillips were together when Steve got the super serum", 
+          "p1": "Colonel Phillips", 
+          "p2": "Dr Erskine"
+        }, 
+        {
+          "desc": "Phillips interrogated Zola to learn Red Skull's plan", 
+          "p1": "Colonel Phillips", 
+          "p2": "Arnim Zola"
+        }, 
+        {
+          "desc": "Dugan and Phillips were both at the planning meeting for the attack on Hydra's last base", 
+          "p1": "Colonel Phillips", 
+          "p2": "Dum Dum Dugan"
+        }, 
+        {
+          "desc": "Jones and Phillips were both at the planning meeting for the attack on Hydra's last base", 
+          "p1": "Colonel Phillips", 
+          "p2": "Gabe Jones"
+        }, 
+        {
+          "desc": "Morita and Phillips were both at the planning meeting for the attack on Hydra's last base", 
+          "p1": "Colonel Phillips", 
+          "p2": "Jim Morita"
+        }, 
+        {
+          "desc": "Falsworth and Phillips were both at the planning meeting for the attack on Hydra's last base", 
+          "p1": "Colonel Phillips", 
+          "p2": "James Falsworth"
+        }, 
+        {
+          "desc": "Dernier and Phillips were both at the planning meeting for the attack on Hydra's last base", 
+          "p1": "Colonel Phillips", 
+          "p2": "Jacques Dernier"
+        }, 
+        {
+          "desc": "Brandt and Phillips were together when Steve got the super serum", 
+          "p1": "Colonel Phillips", 
+          "p2": "Senator Brandt"
+        }, 
+        {
+          "desc": "Red Skull made Erskine give him an unfinished version of the super serum", 
+          "p1": "Red Skull", 
+          "p2": "Dr Erskine"
+        }, 
+        {
+          "desc": "Zola worked for Red Skull in Hydra", 
+          "p1": "Red Skull", 
+          "p2": "Arnim Zola"
+        }, 
+        {
+          "desc": "Dugan rode a zipline into Schmitt's office", 
+          "p1": "Red Skull", 
+          "p2": "Dum Dum Dugan"
+        }, 
+        {
+          "desc": "Falsworth rode a zipline into Schmitt's office", 
+          "p1": "Red Skull", 
+          "p2": "James Falsworth"
+        }, 
+        {
+          "desc": "Jones rode a zipline into Schmitt's office", 
+          "p1": "Red Skull", 
+          "p2": "Gabe Jones"
+        }, 
+        {
+          "desc": "Stark and Erskine worked together to create Captain America", 
+          "p1": "Howard Stark", 
+          "p2": "Dr Erskine"
+        }, 
+        {
+          "desc": "Brandt and Stark were together when Steve got the super serum", 
+          "p1": "Howard Stark", 
+          "p2": "Senator Brandt"
+        }, 
+        {
+          "desc": "Jones was the commando that captured Zola on the train", 
+          "p1": "Arnim Zola", 
+          "p2": "Gabe Jones"
+        }, 
+        {
+          "desc": "Dugan and Jones served together in the howling commandos", 
+          "p1": "Dum Dum Dugan", 
+          "p2": "Gabe Jones"
+        }, 
+        {
+          "desc": "Dugan and Morita served together in the howling commandos", 
+          "p1": "Dum Dum Dugan", 
+          "p2": "Jim Morita"
+        }, 
+        {
+          "desc": "Dugan and Falsworth served together in the howling commandos", 
+          "p1": "Dum Dum Dugan", 
+          "p2": "James Falsworth"
+        }, 
+        {
+          "desc": "Dugan and Dernier served together in the howling commandos", 
+          "p1": "Dum Dum Dugan", 
+          "p2": "Jacques Dernier"
+        }, 
+        {
+          "desc": "Gabe and Jim served together in the howling commandos", 
+          "p1": "Gabe Jones", 
+          "p2": "Jim Morita"
+        }, 
+        {
+          "desc": "Gabe and James served together in the howling commandos", 
+          "p1": "Gabe Jones", 
+          "p2": "James Falsworth"
+        }, 
+        {
+          "desc": "Gabe and Jacque served together in the howling commandos", 
+          "p1": "Gabe Jones", 
+          "p2": "Jacques Dernier"
+        }, 
+        {
+          "desc": "Morita and Falsworth served together in the howling commandos", 
+          "p1": "Jim Morita", 
+          "p2": "James Falsworth"
+        }, 
+        {
+          "desc": "Morita and Dernier served together in the howling commandos", 
+          "p1": "Jim Morita", 
+          "p2": "Jacques Dernier"
+        }, 
+        {
+          "desc": "Falsworth and Dernier served together in the howling commandos", 
+          "p1": "James Falsworth", 
+          "p2": "Jacques Dernier"
+        }
+      ], 
+      "name": "Captain America The First Avenger"
+    }, 
     {
       "characters": [
         "Agent Coulson", 
@@ -164,6 +509,47 @@ return {
         }
       ], 
       "name": "Iron Man"
+    }, 
+    {
+      "characters": [
+        "Agent Blake", 
+        "Agent Sitwell", 
+        "Bennie Pollock", 
+        "Claire Weiss"
+      ], 
+      "interactions": [
+        {
+          "desc": "Claire and Bennie robbed banks together", 
+          "p1": "Claire Weiss", 
+          "p2": "Bennie Pollock"
+        }, 
+        {
+          "desc": "Sitwell recruited Claire into SHIELD", 
+          "p1": "Claire Weiss", 
+          "p2": "Agent Sitwell"
+        }, 
+        {
+          "desc": "Claire became Blake's assistant", 
+          "p1": "Claire Weiss", 
+          "p2": "Agent Blake"
+        }, 
+        {
+          "desc": "Sitwell recruited Bennie into SHIELD", 
+          "p1": "Bennie Pollock", 
+          "p2": "Agent Sitwell"
+        }, 
+        {
+          "desc": "Bennie was with Claire when she became Agent Blake's assistant", 
+          "p1": "Bennie Pollock", 
+          "p2": "Agent Blake"
+        }, 
+        {
+          "desc": "Blake sent Sitwell to retrieve the chitauri gun", 
+          "p1": "Agent Sitwell", 
+          "p2": "Agent Blake"
+        }
+      ], 
+      "name": "Item 47"
     }, 
     {
       "characters": [
