ac0579468ae167171d0518d1e94d7b866ea9cbeb
Alex Lay <AlexChrisLay@gmail.com>
2016-10-17 17:59:41 +0100

    Sorted media by name instead of the os.walk order, puts Iron Man ahead of Iron Man 2

--- CsvToJson.py
+++ CsvToJson.py
@@ -3,7 +3,7 @@ from os import path
 import json
 
 allCharacters = set()
-properties = []
+properties = {}
 categories = set()
 
 for (dirpath, dirnames, filenames) in walk("./interactions"):
@@ -24,22 +24,27 @@ for (dirpath, dirnames, filenames) in walk("./interactions"):
             output = {}
             characters = sorted(characters)
             category = path.basename(dirpath)
-            output['name'] = file[:-4]
+            name = file[:-4]
+            output['name'] = name
             output['category'] = category
             output['characters'] = characters
             output['interactions'] = interactions
             outfile = open(filename[:-3] + 'json','w')
             outfile.write(json.dumps(output, indent=4, sort_keys=True))
             
-            properties.append(output)
+            properties[name] = output
             categories.add(category)
             for character in characters:
                 allCharacters.add(character)
 
+propertylist = []
+for key in sorted(properties.keys()):
+    propertylist.append(properties[key])
+
 output = {}
 output['characters'] = sorted(allCharacters)
 output['categories'] = sorted(categories)
-output['properties'] = properties
+output['properties'] = propertylist
 outfile = open('./interactions/connections.json','w')
 outfile.write(json.dumps(output, indent=2, sort_keys=True))
 
